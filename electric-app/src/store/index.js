c8235944a3e5e39644c3fb475154cbfbac544a81
gearmobile <gearmobile@gmail.com>
2017-10-27 14:06:10 +0300

    electric-app - finish for now

--- electric-app/src/store/index.js
+++ electric-app/src/store/index.js
@@ -72,6 +72,7 @@ const mutations = {
   },
   'CLEAR_ORDER' (state) {
     emptyArray(state.order)
+    // state.commit('CLEAR_SWITCH' // TODO проверить правильность догадки
     state.material = 'brick'
   },
   'CLEAR_SWITCH' (state) {
