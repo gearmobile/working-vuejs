9a97aa12bd100c1b080cfba7e73ee1ef06cbab65
gearmobile <gearmobile@gmail.com>
2017-11-02 09:05:50 +0300

    electric-app - add description component

---
+++ electric-app/src/components/shared/PageDescriptionItem.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+
+  v-list( three-line )
+    v-list-tile
+      v-list-tile-avatar
+        | {{ itemNumber }}
+      v-list-tile-content
+        v-list-tile-title
+          | {{ item.title }}
+        v-list-tile-sub-title
+          | {{ item.subtitle }}
+    v-divider
+
+</template>
+
+<script>
+  export default {
+    name: 'PageDescriptionItem',
+    props: {
+      item: {
+        type: Object,
+        default () { return {} }
+      },
+      itemIndex: {
+        type: Number,
+        default () { return null }
+      }
+    },
+    computed: {
+      itemNumber () {
+        return this.itemIndex + 1
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  //
+</style>
+
