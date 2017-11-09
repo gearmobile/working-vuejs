fca0becf5d8a2194946fafd872eb6c84ad6a0a45
gearmobile <gearmobile@gmail.com>
2017-11-09 15:23:17 +0300

    meetup - fix show edit components

--- meetup-app/src/components/Meetup/MeetupDetails.vue
+++ meetup-app/src/components/Meetup/MeetupDetails.vue
@@ -3,20 +3,33 @@
     v-layout( row )
       v-flex( xs12 )
         v-card
+
           v-card-title
             h5.primary--text.mb-0
               | {{ meetup.title }}
             v-spacer
+            
+            // EDIT MEETUP DETAILS
             template( v-if="isCreator" )
               component-edit( :meetup="meetup" )
+
           v-card-media( :src="meetup.src", height="400px" )
+
           v-card-text
             h6.card-info.info--text.mb-2
               | {{ meetup.date }} {{ meetup.time }} - {{ meetup.location }}
-            component-edit-date( :meetup="meetup" )
-            component-edit-time( :meetup="meetup" )
+            
+            // EDIT MEETUP DATE
+            template( v-if="isCreator" )
+              component-edit-date( :meetup="meetup" )
+            
+            // EDIT MEETUP TIME
+            template( v-if="isCreator" )
+              component-edit-time( :meetup="meetup" )
+            
             p.mb-0.mt-2
               | {{ meetup.description }}
+
           v-card-actions
             v-spacer
             v-btn.primary
