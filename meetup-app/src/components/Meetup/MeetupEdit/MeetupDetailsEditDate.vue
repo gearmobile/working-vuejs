c971d790a5b4a6f3e270459a7525760be6d84d88
gearmobile <gearmobile@gmail.com>
2017-11-09 15:29:38 +0300

    meetup - time to rest

--- meetup-app/src/components/Meetup/MeetupEdit/MeetupDetailsEditDate.vue
+++ meetup-app/src/components/Meetup/MeetupEdit/MeetupDetailsEditDate.vue
@@ -1,24 +1,29 @@
 <template lang="pug">
 
   v-dialog( persistent, v-model="meetupEdit.dialog", max-width="400px" )
+
+    // TRIGGER DIALOG
     v-btn( color="info", dark, slot="activator" )
       | edit date
+
+    // CONTENT SECTION
     v-card
       v-container
-        v-layout( row, wrap )
-          v-flex( xs12 )
-            v-card-title
-              h5.mb-0( style="text-transform: capitalize;" )
-                | edit meetup date
+        
+        v-layout.mb-2
+          v-card-title
+            h5.mb-0
+              | Edit Meetup Date
+        
         v-divider
-        v-layout( row, wrap )
-          v-flex( xs12 )
-            v-date-picker( v-model="meetupEdit.date", actions, style="width: 100%" )
-              template( scope="{ save, cancel }" )
-                v-btn( color="primary darken-3", flat, @click.native="onClose()" )
-                  | cancel
-                v-btn( color="primary darken-3", flat, @click.native="onSave()" )
-                  | save
+        
+        v-layout.mb-2
+          v-date-picker( v-model="meetupEdit.date", actions, style="width: 100%" )
+            template( scope="{ save, cancel }" )
+              v-btn( color="primary darken-3", flat, @click.native="onClose()" )
+                | cancel
+              v-btn( color="primary darken-3", flat, @click.native="onSave()" )
+                | save
 
 </template>
 
@@ -55,8 +60,3 @@
     }
   }
 </script>
-
-<style scoped>
-  /**/
-</style>
-
