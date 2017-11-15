4e66fdb5798ea200b07ca0bfae0c6dfd2a45f184
gearmobile <gearmobile@gmail.com>
2017-11-15 11:03:49 +0300

    meetup - strange behavior - doubling registered meetups in profile

--- meetup-app/src/components/User/Profile.vue
+++ meetup-app/src/components/User/Profile.vue
@@ -8,7 +8,7 @@
     
     v-layout.mb-3( row, v-for="(meetup, index) in meetups", :key="index" )
       v-flex( xs12, sm10, offset-sm1, md8, offset-md2 )
-        v-card.info
+        v-card( color="blue lighten-2" )
           v-container( fluid )
             v-layout( row )
               v-flex( xs5, sm4, md3, v-if="loading", justify-center )
