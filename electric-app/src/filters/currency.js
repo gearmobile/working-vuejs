8a367f1a662a707d7e6d3c247f8372d06ddbb6e0
gearmobile <gearmobile@gmail.com>
2017-11-17 12:39:20 +0300

    move electric-app to ready folder

--- electric-app/src/filters/currency.js
+++
@@ -1,7 +0,0 @@
-// ---------------------------
-// ВЫВОДИТ ЧИСЛА В РУБЛЯХ
-// ---------------------------
-
-export default (value) => {
-  return value.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' })
-}
--- electric-app/src/utils/emptyArray.js
+++
@@ -1,5 +0,0 @@
-export default (arr) => {
-  for (let i = arr.length; i > 0; i -= 1) {
-    arr.pop()
-  }
-}
