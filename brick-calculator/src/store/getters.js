e11fcc6cefc68c56dc1fd95b1b23d3abfc59c638
gearmobile <gearmobile@gmail.com>
2017-12-01 13:49:27 +0300

    brick-app - split store in multiple files - getters, actions, state, mutations

---
+++ brick-calculator/src/store/getters.js
@@ -0,0 +1,145 @@
+import _ from 'lodash'
+import isEmpty from '../utils/isEmpty'
+import { i18n } from '../pluigns/vue-i18n'
+
+const getters = {
+  // GET CURRENT LOCALE
+  getLocale (state) {
+    return i18n.locale
+  },
+  // MASONRY SECTION
+  getMasonry (state) {
+    return state.masonry
+  },
+  getMasonryCurrent (state) {
+    return state.order.masonry
+  },
+  // SEAM SECTION
+  getSeam (state) {
+    return state.seam
+  },
+  getSeamCurrent (state) {
+    return state.order.seam
+  },
+  // BRICK SECTION
+  getBrick (state) {
+    return state.bricks
+  },
+  getBrickCurrent (state) {
+    return state.order.brick
+  },
+  // BUILDING SECTION
+  getBuildingLength (state) {
+    return state.building.length
+  },
+  getBuildingWidth (state) {
+    return state.building.width
+  },
+  getBuildingHeight (state) {
+    return state.building.height
+  },
+  checkFieldsBuilding (state) {
+    return state.building.width !== null && state.building.height !== null && state.building.length !== null
+  },
+  // MASK SECTION
+  getMask (state) {
+    return state.mask
+  },
+  getMaskCounter (state) {
+    return state.mask.length
+  },
+  getMaskOpening (state) {
+    return state.maskOpening
+  },
+  // OPENING SECTION
+  getOpeningText (state) {
+    return state.openingText
+  },
+  getOpeningStatus (state, getters) {
+    return state.openingValueMaxStatus || getters.checkCommonLess
+  },
+  getOpening (state) {
+    return state.opening
+  },
+  getOpeningWidth (state) {
+    return (id) => {
+      return state.opening[id].width
+    }
+  },
+  getOpeningHeight (state) {
+    return (id) => {
+      return state.opening[id].height
+    }
+  },
+  checkFieldsOpening (state) {
+    return state.opening.every(el => {
+      return el.height !== null && el.width !== null
+    })
+  },
+  // CALCULATIONS SECTION
+  getSelectedBrick (state) {
+    const result = _.find(state.bricks, { 'value': state.order.brick })
+    return result
+  },
+  getSeamWidth (state) {
+    return state.order.seam / 1000
+  },
+  getSquareLengthHeight (state, getters) {
+    const result = (getters.getSelectedBrick.length / 1000 + getters.getSeamWidth) * (getters.getSelectedBrick.height / 1000 + getters.getSeamWidth)
+    return result
+  },
+  getSquareWidthHeight (state, getters) {
+    const result = (getters.getSelectedBrick.width / 1000 + getters.getSeamWidth) * (getters.getSelectedBrick.height / 1000 + getters.getSeamWidth)
+    return result
+  },
+  // OUTPUT SECTION
+  getBricksQuantity (state, getters) {
+    let result = null
+    switch (state.order.masonry) {
+      case '0.5':
+        result = Math.ceil(1 / getters.getSquareLengthHeight)
+        break
+      case '1':
+        result = Math.ceil(1 / getters.getSquareWidthHeight)
+        break
+      case '1.5':
+        result = Math.ceil(1 / getters.getSquareLengthHeight) + Math.ceil(1 / getters.getSquareWidthHeight)
+        break
+      case '2':
+        result = (Math.ceil(1 / getters.getSquareWidthHeight)) * 2
+        break
+      case '2.5':
+        result = Math.ceil(1 / getters.getSquareLengthHeight) + (Math.ceil(1 / getters.getSquareWidthHeight)) * 2
+        break
+    }
+    result = (getters.getAreaCommon - getters.getAreaOpening) * result
+    return result
+  },
+  getAreaCommon (state, getters) {
+    if (_.isNil(state.building.length) && _.isNil(state.building.width) && _.isNil(state.building.height)) {
+      return null
+    }
+    const result = ((_.toNumber(state.building.length) + _.toNumber(state.building.width)) * 2) * _.toNumber(state.building.height)
+    return result
+  },
+  getAreaOpening (state) {
+    return state.opening.reduce((total, currentIndex) => {
+      return total + isEmpty(currentIndex.width) * isEmpty(currentIndex.height)
+    }, 0)
+  },
+  checkCommonLess (state, getters) {
+    const common = getters.getAreaCommon === null ? 0 : getters.getAreaCommon
+    if (common !== 0 && common / 2 <= getters.getAreaOpening) {
+      state.openingText = 'Площадь проемов не должна превышать общую площадь строения'
+      return true
+    } else {
+      state.openingText = ''
+      return false
+    }
+  },
+  getBricksCost (state, getters) {
+    return getters.getBricksQuantity * getters.getSelectedBrick.price
+  }
+}
+
+export default getters
---
+++ brick-calculator/src/store/mutations.js
@@ -0,0 +1,96 @@
+const mutations = {
+  'SET_MASONRY' (state, payload) {
+    state.order.masonry = payload
+  },
+  // SEAM SECTION
+  'SET_SEAM' (state, payload) {
+    state.order.seam = payload
+  },
+  'RESET_SEAM_VALUE' (state) {
+    state.order.seam = 0
+  },
+  // BRICK SECTION
+  'SET_BRICK' (state, payload) {
+    state.order.brick = payload
+  },
+  'RESET_BRICK_VALUE' (state) {
+    state.order.brick = 'одинарный'
+  },
+  // BUILDING SECTION
+  'SET_LENGTH' (state, payload) {
+    if (payload === '') {
+      state.building.length = null
+    } else {
+      state.building.length = payload
+    }
+  },
+  'SET_WIDTH' (state, payload) {
+    if (payload === '') {
+      state.building.width = null
+    } else {
+      state.building.width = payload
+    }
+  },
+  'SET_HEIGHT' (state, payload) {
+    if (payload === '') {
+      state.building.height = null
+    } else {
+      state.building.height = payload
+    }
+  },
+  // OPENING SECTION
+  'ADD_COMPONENT' (state) {
+    if (state.opening.length < state.openingValueMax) {
+      state.openingValueMaxStatus = false
+      state.openingText = ''
+      state.opening.push({
+        width: null,
+        height: null
+      })
+    } else {
+      state.openingValueMaxStatus = true
+      state.openingText = 'Вы добавили максимальное количество проемов'
+    }
+  },
+  'REMOVE_COMPONENT' (state, payload) {
+    if (state.opening.length > state.openingValueMin) {
+      state.openingValueMaxStatus = false
+      state.openingText = ''
+      state.opening.splice(payload, 1)
+    } else {
+      state.openingValueMaxStatus = true
+      state.openingText = 'Последний проем удалить нельзя'
+    }
+  },
+  'SET_OPENING_WIDTH' (state, payload) {
+    if (payload.value === '') {
+      state.opening[payload.index].width = null
+    } else {
+      state.opening[payload.index].width = payload.value
+    }
+  },
+  'SET_OPENING_HEIGHT' (state, payload) {
+    if (payload.value === '') {
+      state.opening[payload.index].height = null
+    } else {
+      state.opening[payload.index].height = payload.value
+    }
+  },
+  'CLOSE_ALERT' (state) {
+    state.openingValueMaxStatus = false
+  },
+  'RESET_OPENING_FIELDS' (state) {
+    const arr = state.opening
+    if (arr.length > 1) {
+      for (let i = arr.length; i > 1; i -= 1) {
+        arr.pop()
+      }
+    }
+    arr.forEach(el => {
+      el.width = null
+      el.height = null
+    })
+  }
+}
+
+export default mutations
