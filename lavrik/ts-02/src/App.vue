236540a2a919902da9908fc531975c095389350f
gearmobile <gearmobile@gmail.com>
2018-01-24 11:59:23 +0300

    tz02

---
+++ lavrik/ts-02/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!--  -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        // msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    beforeCreate () {
+      console.log(`before create hook`)
+    },
+    created () {
+      console.log(`created hook`)
+    },
+    beforeMount () {
+      console.log(`before mount hook`)
+    },
+    mounted () {
+      console.log(`mounted hook`)
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
