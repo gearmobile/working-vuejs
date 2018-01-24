19623c71285445de4c7c29f93980c9b0236da2af
gearmobile <gearmobile@gmail.com>
2018-01-24 13:11:02 +0300

    lavrik

--- lavrik/ts-02/src/App.vue
+++ lavrik/ts-02/src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="app">
-    <!--  -->
+  <div class="container my-4">
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="name">name</label>
+          <input class="form-control" type="text" name="name" id="name" v-model="name">
+        </div>
+        <div class="form-group">
+          <label for="phone">phone</label>
+          <input class="form-control" type="text" name="phone" id="phone" v-model="name">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,20 +20,33 @@
     name: 'app',
     data () {
       return {
-        // msg: 'Welcome to Your Vue.js App'
+        name: ''
       }
     },
     beforeCreate () {
       console.log(`before create hook`)
+      console.log(this.$el)
     },
     created () {
       console.log(`created hook`)
+      console.log(this.$el)
     },
     beforeMount () {
       console.log(`before mount hook`)
+      console.log(this.$el)
+      console.log(this.$el.innerHTML)
     },
     mounted () {
       console.log(`mounted hook`)
+      console.log(this.$el)
+      console.log(this.$el.innerHTML)
+    },
+    beforeUpdate () {
+      console.log(`before update`)
+      if (/^[0-9]*$/.test(this.phone))
+    },
+    updated () {
+      console.log(`updated`)
     }
   }
 </script>
